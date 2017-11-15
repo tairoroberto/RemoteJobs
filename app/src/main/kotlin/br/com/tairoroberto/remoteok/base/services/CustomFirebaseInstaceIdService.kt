d945ebd72c9891b17eb3e6629dd191944d14ecfa
tairoroberto <tairoroberto@hotmail.com>
2017-11-15 05:21:20 -0200

    Add pish notification

---
+++ app/src/main/kotlin/br/com/tairoroberto/remoteok/base/services/CustomFirebaseInstaceIdService.kt
@@ -0,0 +1,23 @@
+package br.com.tairoroberto.remoteok.base.services
+
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceIdService
+import com.google.firebase.iid.FirebaseInstanceId
+
+/**
+ * Created by tairo on 9/8/17.
+ */
+class CustomFirebaseInstaceIdService: FirebaseInstanceIdService() {
+    private val TAG = "LOG"
+
+    override fun onTokenRefresh() {
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.i(TAG, "onTokenRefresh: " + refreshedToken)
+
+        sendRegistrationToServer(refreshedToken)
+    }
+
+    private fun sendRegistrationToServer(refreshedToken: String?) {
+
+    }
+}
---
+++ app/src/main/kotlin/br/com/tairoroberto/remoteok/base/services/CustomFirebaseMessagingService.kt
@@ -0,0 +1,60 @@
+package br.com.tairoroberto.remoteok.base.services
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import android.support.v4.app.NotificationCompat
+import android.support.v4.content.ContextCompat
+import android.text.TextUtils
+import br.com.tairoroberto.remoteok.R
+import br.com.tairoroberto.remoteok.splash.SplashActivity
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+/**
+ * Created by tairo on 9/8/17.
+ */
+class CustomFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        if (remoteMessage?.data?.isNotEmpty() == true) {
+
+            val parametroScreen = remoteMessage.data["screen"]
+
+            if (!TextUtils.isEmpty(parametroScreen)) {
+                showNotification("screen", parametroScreen)
+            }
+        }
+
+        if (remoteMessage?.notification != null) {
+            showNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+        }
+    }
+
+    private fun showNotification(title: String?, body: String?) {
+        val intent = Intent(this, SplashActivity::class.java)
+
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val builder = NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.ic_send_black_24dp)
+                .setColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        builder.setSound(defaultSoundUri)
+
+        val notification = builder.build()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notification)
+    }
+}
