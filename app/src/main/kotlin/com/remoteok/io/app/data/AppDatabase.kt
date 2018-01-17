1f7d7884121695a14609e49db0bb38dd2d04615e
tairoroberto <tairoroberto@hotmail.com>
2018-01-16 22:43:40 -0200

    Adjust migration

--- app/src/main/kotlin/com/remoteok/io/app/data/AppDatabase.kt
+++ app/src/main/kotlin/com/remoteok/io/app/data/AppDatabase.kt
@@ -7,8 +7,12 @@ import android.arch.persistence.room.TypeConverters
 import android.content.Context
 import com.remoteok.io.app.data.dao.JobsDao
 import com.remoteok.io.app.model.Job
+import android.arch.persistence.db.SupportSQLiteDatabase
+import android.arch.persistence.room.migration.Migration
 
-@Database(entities = [(Job::class)], version = 2)
+
+
+@Database(entities = [(Job::class)], version = 3)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
@@ -23,9 +27,17 @@ abstract class AppDatabase : RoomDatabase() {
                     INSTANCE ?: buildDatabase(context as Context).also { INSTANCE = it }
                 }
 
+
+        private val MIGRATION_1_3: Migration = object : Migration(1, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // Since we didn't alter the table, there's nothing else to do here.
+            }
+        }
+
         private fun buildDatabase(context: Context) =
                 Room.databaseBuilder(context.applicationContext,
                         AppDatabase::class.java, "remoteok.db")
+                        .addMigrations(MIGRATION_1_3)
                         .build()
     }
 }
