6e5938cc4d0dee7b992e7872907f67a205e78228
tairoroberto <tairoroberto@hotmail.com>
2018-05-12 23:20:41 -0300

    add instant app module

---
+++ base/src/androidTest/java/com/remotejobs/ui/MainActivityTest.kt
@@ -0,0 +1,48 @@
+package com.remotejobs.ui
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import com.remotejobs.ApplicationTest
+import com.remotejobs.io.app.R
+import com.remotejobs.io.app.view.home.HomeFragment
+import com.remotejobs.io.app.view.home.MainActivity
+import org.hamcrest.CoreMatchers
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule
+    @JvmField
+    var serverRule = InstrumentedTestRequestMatcherRule()
+
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+    val app = InstrumentationRegistry.getTargetContext().applicationContext as ApplicationTest
+
+    @Before
+    fun setUp() {
+        activityRule.activity.supportFragmentManager.beginTransaction().replace(R.id.container, HomeFragment(), "home")
+    }
+
+    @Test
+    fun sholdListJobs() {
+        serverRule.addFixture(200, "remote-jobs.json")
+                .ifRequestMatches()
+                .methodIs(HttpMethod.GET)
+                .pathMatches(CoreMatchers.endsWith("/fetchJobs"))
+
+        activityRule.launchActivity(null)
+
+
+        onView(withText("Dev")).check(matches(isDisplayed()))
+    }
+}
---
+++ base/src/main/java/com/remotejobs/io/app/viewmodel/detail/DetailViewModel.kt
@@ -0,0 +1,20 @@
+package com.remotejobs.io.app.viewmodel.detail
+
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import com.remotejobs.io.app.model.Job
+
+/**
+ * Created by tairo on 12/12/17.
+ */
+class DetailViewModel : ViewModel() {
+
+    fun manipulateResponse(job: Job?) {
+        Log.i("TAG", "job ${job?.position}")
+
+    }
+
+    fun showError(str: String) {
+
+    }
+}
