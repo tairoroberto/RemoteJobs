c0149847aa4b04363484c25f437f91726da84c8f
vrx_tassuncao <vrx_tassuncao@uolinc.com>
2018-10-16 16:34:37 -0300

    add test

--- base/src/androidTest/java/com/remotejobs/io/app/ui/MainActivityTest.kt
+++ base/src/androidTest/java/com/remotejobs/io/app/ui/MainActivityTest.kt
@@ -1,39 +1,16 @@
 package com.remotejobs.io.app.ui
 
 import androidx.test.InstrumentationRegistry
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import br.com.concretesolutions.requestmatcher.InstrumentedTestRequestMatcherRule
-import com.remotejobs.io.app.view.BaseActivity
-import org.hamcrest.CoreMatchers
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-    @Rule
-    @JvmField
-    var serverRule = InstrumentedTestRequestMatcherRule()
-
-    @get:Rule
-    var activityRule = ActivityTestRule(BaseActivity::class.java, true, false)
-
-    val app = InstrumentationRegistry.getTargetContext().applicationContext
 
     @Test
-    fun sholdListJobs() {
-        serverRule.addFixture(200, "remote-jobs.json")
-                .ifRequestMatches()
-                .pathMatches(CoreMatchers.endsWith("/fetchJobs"))
-
-        activityRule.launchActivity(null)
-
-
-        onView(withText("Dev")).check(matches(isDisplayed()))
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
     }
 }
