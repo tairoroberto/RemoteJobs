44ab8a500080a78678d2508d04e58e79b5023dc7
vrx_tassuncao <vrx_tassuncao@uolinc.com>
2018-10-17 10:42:05 -0300

    add test splash

--- splash/src/androidTest/java/com/remotejobs/io/app/splash/ExampleInstrumentedTest.kt
+++ splash/src/androidTest/java/com/remotejobs/io/app/splash/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.remotejobs.io.app.splash", appContext.packageName)
+        assertEquals("com.remotejobs.io.app.splash.test", appContext.packageName)
     }
 }
